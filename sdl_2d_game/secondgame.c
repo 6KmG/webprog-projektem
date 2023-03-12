13f134c4268f130eae44bd275cedff1833bef496
6kmg <ij6zhu@gmail.com>
2023-03-12 12:25:35 +0100

    usual commit

--- sdl_2d_game/secondgame.c
+++ sdl_2d_game/secondgame.c
@@ -4,16 +4,31 @@
 #include <time.h>
 #include <math.h>
 
-#define WIDTH 800
-#define HEIGHT 480
+// next to do:
+// gravity
+// collision detection
+// jumpingf
+
 #define true 1
 #define false 0
 #define bool char
-#define FPS 60
+
+#define WIDTH 800
+#define HEIGHT 480
+#define SPEED 5
+#define FPS 240
+#define BACKGROUNDCOLOR 101,154,210,255
+#define PLAYERCOLOR 255,255,255,255
 
 int main(int argc, char *argv[])
 {
+    float velocity = 0;
+    bool pressD = false;
+    bool pressA = false;
+    bool pressSpace = false;
+
     SDL_Init(SDL_INIT_EVERYTHING);
+
     SDL_Window *window = SDL_CreateWindow(
         "TestTitle",
         SDL_WINDOWPOS_UNDEFINED,
@@ -21,27 +36,60 @@ int main(int argc, char *argv[])
         WIDTH,
         HEIGHT,
         0
-        );
-
+    );
     
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_Rect player;
+    player.h = 30;
+    player.w = 30;
+    player.x = 30;
+    player.y = 30;
 
     SDL_Event event;
     bool running = true;
     while(running)
     {
+        SDL_SetRenderDrawColor(renderer, BACKGROUNDCOLOR);
+        SDL_RenderClear(renderer); 
 
-    if(SDL_PollEvent(&event))
-    {
-        switch(event.type)
-        {
-            // Close app on the exit button
-            case SDL_QUIT:
-                running = 0;
-                break;
+        if(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                case SDL_KEYDOWN:
+                    if(event.key.keysym.sym == SDLK_d) pressD = true;
+                    if(event.key.keysym.sym == SDLK_a) pressA = true;
+                    if(event.key.keysym.sym == SDLK_SPACE) pressSpace = true;
+                    break;
+                case SDL_KEYUP:
+                    if(event.key.keysym.sym == SDLK_d) pressD = false;
+                    if(event.key.keysym.sym == SDLK_a) pressA = false;
+                    if(event.key.keysym.sym == SDLK_SPACE) pressSpace = false;
+                    break;
+            }
         }
+
+        if(pressD) velocity += (float) 60 / FPS;
+        if(pressA) velocity -= (float) 60 / FPS;
+
+        if(velocity >= 1) {
+            player.x += round(velocity) + SPEED;
+            velocity = 0;
+        }
+        if(velocity <= -1){
+            player.x += round(velocity) - SPEED;
+            velocity = 0;
+        }
+
+        SDL_SetRenderDrawColor(renderer, PLAYERCOLOR);
+        SDL_RenderFillRect(renderer, &player);
+
+        SDL_RenderPresent(renderer); 
+        SDL_Delay(1000/FPS);
     }
 
-    SDL_Delay(1000/FPS);
-    }
-    return 0;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 }
