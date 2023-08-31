59aa2bcdf1c9d66c2c435cea4bfc1cc712c8440f
6kmg <ij6zhu@gmail.com>
2023-08-31 13:43:00 +0200

    usual commit

--- Python/autoagentchooser.py
+++ Python/autoagentchooser.py
@@ -1,11 +1,21 @@
 import time
 import pyautogui
-import random
+import ctypes
 
-keys = ['w','a','s','d']
+user32 = ctypes.windll.user32
+user32.SetProcessDPIAware()
+
+x = user32.GetSystemMetrics(0) / 1280
+y = user32.GetSystemMetrics(1) / 720
 
 while True:
-    a = random.choice(keys)
-    pyautogui.keyDown(a)
-    time.sleep(0.5)
-    pyautogui.keyUp(a)
+    pyautogui.moveTo(630 * x, 675 * y, 0.5)
+    pyautogui.click()
+    
+    pyautogui.moveTo(475 * x, 675 * y, 0.5)
+    pyautogui.click()
+    
+    pyautogui.moveTo(800 * x, 600 * y, 0.5)
+    pyautogui.click()
+    
+    time.sleep(3)
