71a6219fac6910da8008a39f3e75fe524ca576a1
6kmg <ij6zhu@gmail.com>
2023-09-01 22:03:12 +0200

    usual commit

--- asm/testnasm.asm
+++ asm/testnasm.asm
@@ -1,1 +1,48 @@
-.global _start
+global _main
+
+extern _ExitProcess@4
+extern _MessageBoxA@16
+
+section .data
+    title db "Title", 0
+    msg db "Please choose yes or no.", 0
+
+    yes_msg db "You choosed yes.", 0
+    no_msg db "You choosed no."
+
+section .text
+_main:
+    push 0x040034
+    push title
+    push msg
+    push 0
+    call _MessageBoxA@16
+
+    cmp eax, 6
+    je yes
+
+    cmp eax, 7
+    je no
+
+    push 1
+    call _ExitProcess@4
+
+yes:
+    push 0x040000
+    push title
+    push yes_msg
+    push 0
+    call _MessageBoxA@16
+
+    push 0
+    call _ExitProcess@4
+
+no:
+    push 0x040000
+    push title
+    push no_msg
+    push 0
+    call _MessageBoxA@16
+
+    push 0
+    call _ExitProcess@4
