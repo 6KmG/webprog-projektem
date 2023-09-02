ede2b2157586a856608b911fd7caba9329d42966
6kmg <ij6zhu@gmail.com>
2023-09-02 13:26:59 +0200

    usual commit

--- Python/autoagentchooser.py
+++ Python/autoagentchooser.py
@@ -9,6 +9,8 @@ x = user32.GetSystemMetrics(0) / 1280
 y = user32.GetSystemMetrics(1) / 720
 
 while True:
+    time.sleep(20)
+    
     pyautogui.moveTo(630 * x, 675 * y, 0.5)
     pyautogui.click()
     
@@ -17,5 +19,3 @@ while True:
     
     pyautogui.moveTo(800 * x, 600 * y, 0.5)
     pyautogui.click()
-    
-    time.sleep(3)
