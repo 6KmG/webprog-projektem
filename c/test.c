83ffad1d7171555dc24152f889e2460cae81a341
6kmg <ij6zhu@gmail.com>
2023-09-02 11:41:32 +0200

    modified:   c/test.c 	modified:   c/test.exe 	deleted:    c/test.o 	modified:   c/test.obj

--- c/test.c
+++ c/test.c
@@ -1,16 +1,67 @@
 #include <windows.h>
-<<<<<<< HEAD
-int _main(){
-    MessageBox(0, 0, 0, 0);
-    ExitProcess(0);
-}
-=======
 
-int __main(){
-    int diag = MessageBox(0, "hi", "hi", MB_YESNOCANCEL|MB_ICONWARNING);
-    while(diag == IDYES) {
-        diag = MessageBox(0, "hi", "hi", MB_YESNOCANCEL|MB_ICONERROR);
+#define WIDTH 800
+#define HEIGHT 600
+#define TITLE "The title of the window"
+
+const char className[] = "class69";
+
+// Step 4: the Window Procedure
+int __stdcall WndProc(
+    HWND hwnd, 
+    unsigned int msg, 
+    unsigned int wParam, 
+    int *lParam)
+{
+    switch(msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+        break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProcA(hwnd, msg, wParam, lParam);
     }
-    ExitProcess(0);
-} 
->>>>>>> e3fb36dbbbfd4271d371da7f1eaeb81c9bb9cf55
+    return 0;
+}
+
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    char* lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEXA wc;
+    HWND hwnd;
+    MSG Msg;
+
+    //Step 1: Registering the Window Class
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = className;
+    wc.hbrBackground = (HBRUSH)(17);
+    wc.hIconSm       = LoadIconA(NULL, IDI_APPLICATION);
+
+    RegisterClassExA(&wc);
+
+    // Step 2: Creating the Window
+    hwnd = CreateWindowExA(
+        0,
+        className,
+        TITLE,
+        WS_OVERLAPPEDWINDOW,
+        0, 0, 
+        WIDTH, HEIGHT,
+        0, 0, hInstance, NULL);
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    // Step 3: The Message Loop
+    while(GetMessageA(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessageA(&Msg);
+    }
+}
