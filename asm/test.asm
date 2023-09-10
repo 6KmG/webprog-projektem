a0eacfb684d6245bbf54d8f7df604c0b94da15a5
6kmg <ij6zhu@gmail.com>
2023-09-10 12:18:50 +0200

    new file:   asm/mllink$.lnk 	modified:   asm/test.asm 	modified:   asm/test.exe 	modified:   asm/test.obj

--- asm/test.asm
+++ asm/test.asm
@@ -1,35 +1,16 @@
-.386
-.model flat, stdcall
-option casemap :none
+extern ExitProcess:proc
+extern WriteConsoleA:proc 
+extern GetStdHandle:proc
 
-include \masm32\include\kernel32.inc
-include \masm32\include\user32.inc
-includelib \masm32\lib\kernel32.lib
-includelib \masm32\lib\user32.lib
+option casemap:none
 
 .data
-    ; message db 0
-    ; titlebar db 0
+    msg db "Hello world!",10,0
 
 .code
-main:
-    ;invoke MessageBox, 0, addr message, addr titlebar, 0
-    ; xor edx, edx
-    ; mov eax, 21
-    ; mov ebx, 8
-    ; idiv ebx
-    
-    ; push edx
-    ; push offset format
-    ; push offset message
-    ; call wsprintfA
-
-    push 0
-    push 0 ; offset titlebar
-    push 0 ; offset message
-    push 0
-    call MessageBox
-
-    push 0
+main proc
+    push 5
+    pop rcx
     call ExitProcess
-end main
+main endp
+end
