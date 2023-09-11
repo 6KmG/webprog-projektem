6a7836237b5fc2e69073b6621644347bf83576c3
6kmg <ij6zhu@gmail.com>
2023-09-11 11:53:09 +0200

    modified:   asm/test.asm 	modified:   asm/test.exe 	modified:   asm/test.obj 	new file:   c/a.exe 	modified:   c/test.exe 	modified:   c/test2.exe 	modified:   c/test2.s

--- asm/test.asm
+++ asm/test.asm
@@ -4,34 +4,29 @@ extern GetStdHandle:proc
 extern MessageBoxA:proc
 
 .data
-    msg db "Hello world!",0
+    msg db "Hello world!",10,0
+    len equ $-msg
     titl db "TItle",0
 
 .code
 main proc
     push rbp
     mov rbp, rsp
-    sub rsp, 1024
+    sub rsp, 128
+
+    mov rcx, 0
+    mov rdx, offset msg
+    mov r8, offset titl
+    mov r9, 0
+    call MessageBoxA
 
     mov rcx, -11
     call GetStdHandle
-    
-    ; mov rcx, 0
-    ; mov rdx, offset titl
-    ; mov r8, offset msg
-    ; mov r9, 0
-    ; call MessageBoxA
 
-    mov rbx, 0
-    mov rax, 0FFFFFFh
-    looping:
-        inc rbx
-        cmp rbx, rax
-        jl looping
-
-    mov rcx, rcx            ; GetStdHandle return
+    push 0
+    mov rcx, rax            ; GetStdHandle return
     mov rdx, offset msg     ; Pointer to the string
-    mov r8, 15              ; Length of the string
+    mov r8, len              ; Length of the string
     mov r9, 0               ; 4th parameter
     call WriteConsoleA
 
