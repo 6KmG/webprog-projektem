8fb47b1ba044850f827da2a4b279d9bfdef02837
6kmg <ij6zhu@gmail.com>
2023-10-06 11:06:00 +0200

    usual commit

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <thread>
+#include <math.h>
+#include <chrono>
+#include <vector>
+#include <exception>
+#include <cstring>
+#include <windows.h>
 
-int* length(int *array[]){
-    int *p = array;
-    return p + 1;
+#define BLOCK_SIZE 1000
+#define THREADS 1000
+
+void stressCPU(){
+    while(true){
+        Sleep(0.00000001);
+    }
 }
 
 int main(){
-    int a[6];
-    printf("%d\n", length(&a));
-    printf("%d\n", &a);
-    // *(&a + 1) - a
+    int sum = 0;
+    int arrayPrime[THREADS];
+    memset(arrayPrime, 0, sizeof(int) * THREADS);
+    auto start = std::chrono::high_resolution_clock::now();
+
+    try{
+        std::vector<std::thread> threads;
+
+        for(int i = 0; i < THREADS; i++){
+            threads.push_back(std::thread(stressCPU));
+        }
+        for(int i = 0; i < threads.size(); i++){
+            threads[i].join();
+        }
+    }catch(std::system_error error){
+        printf("%s\n", error.what());
+    }
+
+    for(int i = 0; i < THREADS; i++){
+        sum += arrayPrime[i];
+    }
+    printf("%d\n", sum);
+
+    auto end = std::chrono::high_resolution_clock::now();    
+    std::chrono::duration<double, std::milli> elapsed = (end - start) / 1000;
+    printf("Program finished in %.4f seconds.\n", elapsed.count());
 }
--- dos/C/test.asm
+++ dos/C/test.asm
@@ -1,34 +1,31 @@
 .MODEL small 
 .STACK 
 .data
-    msg db "sadas",0
+    msg db ?
+    errorMessage db "Exception!",0
+
 
 .CODE
-printChar proc
-    push ax
+exitProcess proc
+    mov ah, 4Ch
+    int 21h
+exitProcess endp
 
+printChar proc
     MOV AH, 2
     INT 21h
-
-    pop ax
     ret
 printChar endp
 
 readChar proc
-    push ax
-
     mov ah, 01h
     int 21h
-    xor bx, bx
     mov dl, al
 
-    pop ax
     ret
 readChar endp 
 
 print proc
-    push ax
-
     mov dl, [bx]
     cmp dl, 0
     je exit
@@ -41,13 +38,10 @@ print proc
     exit:
         mov dl, 0
         call printChar
-        pop ax
         ret
 print endp
 
 printLine proc
-    push ax
-
     looping:
         mov dl, [bx]
         cmp dl, 0
@@ -61,22 +55,79 @@ printLine proc
     exit:
         mov dl, 10
         call printChar
-
-        pop ax
         ret
 printLine endp
 
+read proc
+    looping:
+        call readChar
+        cmp dl, " "
+        je exit
+        mov byte ptr [bx], dl 
+        inc bx
+
+    jmp looping
+
+    exit: 
+        mov byte ptr [bx], 0
+        ret
+read endp 
+
+exception proc
+    lea bx, errorMessage 
+    call print
+    mov ax, 1
+    ret
+exception endp
+
+readInt proc
+    push bx
+    call read
+    pop bx
+
+    xor ax, ax
+    xor dx, dx
+
+    looping:
+        mov al, byte ptr [bx] ; "6" "5"
+
+
+        
+        cmp al, "9"
+        jg error1
+
+        sub ax, "0" ; 6 5
+        add ax, dx  ; ax: 6 + 0 = 6 ax: 5 + 60 = 65
+        mov cx, 10
+        mul cx ; ax: 6 * 10 = 60
+
+        mov dx, ax ; dx : 60
+
+        inc bx ; byte ptr [bx] = "3"
+        jmp looping
+
+    error1:
+        call exception
+        ret
+
+    exitcode:
+        mov ax, dx
+        mov dl, 10
+        idiv dl
+        mov dx, ax
+        ret
+readInt endp
 
 main proc
     mov ax, DGROUP
     mov ds, ax
 
     ; code here
-
     lea bx, msg
-    call print
- 
-    mov ah, 4Ch
-    int 21h
+    call readInt
+
+    call printChar
+
+    call exitProcess
 main endp
 END main
